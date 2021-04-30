a2b4aabe7aeb7555c56aa0523cc121e7f0b33539
Lalu Abdurrahman <lalurahmanms@gmail.com>
2021-04-30 22:53:57 +0800

    add index.php

--- application/config/config.php
+++ application/config/config.php
@@ -40,7 +40,7 @@ $config['base_url'] = 'https://sisfosmanjuhbursel.space/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
