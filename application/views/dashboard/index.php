28074cae7f684402c26201131e96688f7d204f92
fandiiksan <2fandiiksan@gmail.com>
2021-05-17 13:51:43 +0700

    baru nav

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -2,15 +2,9 @@
     <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
-        <span>
-        <h5 style="font-family:perpetua; align:center;" > (60101019) Jln.Anthon Lesnussa, Wenono, Kec.Namrole, Kab.Buru Selatan Prov.Maluku. Pos : 97573</h5>
-        </span>
-        <span>
-                <h5 style="color:#f0f2f5;">...</h5><br>
-        </span>
-        <span>
-                <h5 style="font-family:perpetua; align:center; font-style:italic;"> Email : smanegerinamrole@yahoo.com</h5><br>
-        </span>
+        <span><h5 style="font-family:perpetua; align:center;" > (60101019) Jln.Anthon Lesnussa, Wenono, Kec.Namrole, Kab.Buru Selatan Prov.Maluku. Pos : 97573</h5></span>
+        <span><h5 style="color:#f0f2f5;">...</h5><br></span>
+        <span><h5 style="font-family:perpetua; align:center; font-style:italic;"> Email : smanegerinamrole@yahoo.com</h5><br></span>
             <div class="col-lg-6 col-12">
                 <!-- small box -->
                 <div class="small-box bg-info">
